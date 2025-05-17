2604309bc3ff80e2a0f2913cd163402099d12e20
HB-Hridoy <44264453+HB-Hridoy@users.noreply.github.com>
2025-05-17 16:36:44 +0200

    feat: add default theme icon

--- src/utils/semanticTable.js
+++ src/utils/semanticTable.js
@@ -148,6 +148,12 @@ class SemanticTable {
     return cell;
   }
 
+  #createDefaultThemeIcon(){
+    return `<svg id="default-theme-icon" class="w-4 h-4 text-blue-600 me-2" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+                              </svg>`
+  }
+
   // Create edit cell
   #createEditCell({ semanticId }) {
     const cell = document.createElement('td');
@@ -283,6 +289,9 @@ class SemanticTable {
       newHeader.setAttribute('default-theme', true);
     }
     newHeader.innerHTML = ` <div class="w-full flex items-center relative">
+
+                            ${defaultTheme === themeName ? this.#createDefaultThemeIcon() : ''}
+
                               <p id="theme-${themeName}" class="flex-1 text-xs mr-1">${themeName}</p>
                               <button id="theme-edit-button-${themeName}" 
                                 class="theme-edit-button items-center ml-1">
@@ -293,6 +302,7 @@ class SemanticTable {
 
                                 <span class="sr-only">Edit</span>
                               </button>
+
                             </div>
                           `;
 
@@ -439,6 +449,18 @@ class SemanticTable {
     }
   
     newDefaultHeader.setAttribute('default-theme', true);
+
+    // Move the default theme icon to the new default theme header
+    const defaultThemeIcon = this.#createDefaultThemeIcon();
+    const currentDefaultIcon = currentDefaultHeader?.querySelector('#default-theme-icon');
+    if (currentDefaultIcon) {
+      currentDefaultIcon.remove();
+    }
+
+    const newDefaultIconContainer = newDefaultHeader.querySelector('.w-full.flex.items-center.relative');
+    if (newDefaultIconContainer) {
+      newDefaultIconContainer.insertAdjacentHTML('afterbegin', defaultThemeIcon);
+    }
   
     console.log(`[SEMANTIC TABLE] Default theme mode set to "${themeName}".`);
     return true;
