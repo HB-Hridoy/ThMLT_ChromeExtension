4180f3390db4d29e076e971b158721fae09f2344
HB-Hridoy <44264453+HB-Hridoy@users.noreply.github.com>
2025-05-19 17:13:43 +0200

    feat: add font cache

--- src/utils/cache/cacheManager.js
+++ src/utils/cache/cacheManager.js
@@ -1,18 +1,21 @@
 import ProjectCache from './projectCache.js';
 import PrimitiveCache from './primitiveCache.js';
 import SemanticCache from './semanticCache.js';
+import FontsCache from './fontsCache.js';
 
 class CacheManager {
   constructor() {
     this.projects = new ProjectCache();
     this.primitives = new PrimitiveCache();
     this.semantics = new SemanticCache();
+    this.fonts = new FontsCache();
   }
 
   clearAll() {
     this.projects.clear();
     this.primitives.clear();
     this.semantics.clear();
+    this.fonts.clear();
   }
 }
 
