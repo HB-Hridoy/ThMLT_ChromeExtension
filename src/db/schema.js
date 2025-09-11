0d7261bd1386373eee7adfc4d8ca0196eda5954b
HB-Hridoy <44264453+HB-Hridoy@users.noreply.github.com>
2025-09-11 12:25:07 +0200

    feat(db): upgrade Dexie schema to v2 and add typography table

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,5 +1,5 @@
 const ThMLT_DB_Schema = {
-  dbName: "ThMLTDatabase-Alpha2",
+  dbName: "ThMLTDatabase",
   version: 2,
   stores: {
     projects: "++projectId, projectName, deleted, deletedAt, [deleted+deletedAt], lastModified",
